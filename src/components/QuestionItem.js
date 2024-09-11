1d1544252dc1eec8359b9a9a896b7861cbb109ee
heeba <heebahassan182@gmail.com>
2024-09-11 18:04:01 +0300

    completed labs

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onUpdate, onDelete }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,17 +9,40 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => onDelete(id));
+  }
+
+  function handleChange(event) {
+    const updatedIndex = parseInt(event.target.value, 10);
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex: updatedIndex }),
+    })
+      .then(response => response.json())
+      .then(updatedQuestion => onUpdate(updatedQuestion));
+  }
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select value={correctIndex} onChange={handleChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
 
 export default QuestionItem;
+
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,12 +1,23 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, onUpdateQuestion, onDeleteQuestion }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map(question => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onUpdate={onUpdateQuestion}
+            onDelete={onDeleteQuestion}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
 
 export default QuestionList;
+
